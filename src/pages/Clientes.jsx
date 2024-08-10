67151ea47ccb1aad151569d72c75c77291e6e68c
OverCode <avalosfigueroapedro@gmail.com>
2024-08-09 19:17:01 -0500

    feat: implement lazy loading for images in Clientes page

--- src/pages/Clientes.jsx
+++ src/pages/Clientes.jsx
@@ -1,12 +1,27 @@
 import { Navbar } from "../components/ui/Navbar"
 import { CardClientsLarge } from "../components/common/CardClientsLarge"
 import { CardClientsSquare } from "../components/common/CardClientsSquare"
+import { ImageModal } from "../components/ui/ImageModal"
 import { Footer } from "../components/ui/Footer"
+import { useState } from 'react'
 import { CLIENTES_LARGE, CLIENTES_SQUARE, IMAGES_TRABAJOS } from "../constants/dataConstants"
 
 import '../styles/pages/Clientes.css'
 
 export function Clientes () {
+  const [modalOpen, setModalOpen] = useState(false)
+  const [imageModal, setImageModal] = useState('')
+
+  const handleModal = (image) => {
+    setModalOpen(!modalOpen)
+    setImageModal(image)
+  }
+
+  const closeModal = () => {
+    setImageModal('')
+    setModalOpen(false)
+  }
+
   return (
     <>
       <Navbar />
@@ -35,13 +50,17 @@ export function Clientes () {
             <p><span>Trabajo realizado: </span>{trabajo.trabajo}</p>
             <div className='gallery-trabajo'>
               {trabajo.images.map((image, index) => (
-                <img loading='lazy' key={index} src={image} />
+                <img loading='lazy' key={index} src={image} onClick={() => handleModal(image)}/>
               ))}
             </div>
           </div>
         ))}
       </section>
       <Footer />
+
+      {modalOpen && (
+        <ImageModal image={imageModal} closeModal={closeModal} />
+      )}
     </>
   )
 }
